f1887b3cd2331202ebb7ca028469a5ea90011c6b
VARUN AHUJA <varunahuja.20@gmail.com>
2023-02-21 01:02:26 +0530

    added two comp for number and date fields and added expiryDate field

--- client/src/VendorDetails/CreateVendorOrder.tsx
+++ client/src/VendorDetails/CreateVendorOrder.tsx
@@ -1,4 +1,4 @@
-import { Autocomplete, Grid, TextField } from "@mui/material";
+import { Autocomplete, TextField } from "@mui/material";
 import { Field, FieldArray, Form, Formik } from "formik";
 import React from "react";
 import * as yup from "yup";
@@ -35,6 +35,69 @@ const inputTextField = (props: any) => {
   );
 };
 
+const inputNumberField = (props: any) => {
+  console.log(props);
+  const {
+    form: { errors, touched },
+    index,
+    id
+  } = props;
+  console.log(errors, touched);
+  let [item, , name] = props.field.name.split(".");
+  const err =
+    errors && errors[item] && errors[item][index] && errors[item][index][name];
+  const touch =
+    touched &&
+    touched[item] &&
+    touched[item][index] &&
+    touched[item][index][name];
+
+  return (
+    <TextField
+      fullWidth
+      id={id}
+      name={props.field.name}
+      label={props.label}
+      type="number"
+      // value={props.field.name}
+      onChange={props.field.onChange}
+      error={err && touch}
+      // helperText={formik.touched.saltName && formik.errors.saltName}
+    />
+  );
+};
+
+const inputDateField = (props: any) => {
+  console.log(props);
+  const {
+    form: { errors, touched },
+    index,
+    id
+  } = props;
+  console.log(errors, touched);
+  let [item, , name] = props.field.name.split(".");
+  const err =
+    errors && errors[item] && errors[item][index] && errors[item][index][name];
+  const touch =
+    touched &&
+    touched[item] &&
+    touched[item][index] &&
+    touched[item][index][name];
+
+  return (
+    <TextField
+      fullWidth
+      id={id}
+      name={props.field.name}
+      label={props.label}
+      type="date"
+      // value={props.field.name}
+      onChange={props.field.onChange}
+      error={err && touch}
+      // helperText={formik.touched.saltName && formik.errors.saltName}
+    />
+  );
+};
 const CreateOrder = () => {
   const schema = yup.object().shape({
     itemList: yup.array().of(
@@ -43,6 +106,7 @@ const CreateOrder = () => {
         hsnCode: yup.string().required("Required"),
         companyName: yup.string().required("Required"),
         batchNumber: yup.string().required("Required"),
+        expiryDate: yup.date().required("Required"),
         availableQuantity: yup.number().required("Required").test(
           'Is positive?', 
           'ERROR: The quantity value cannot be less than 0!', 
@@ -73,6 +137,7 @@ const CreateOrder = () => {
     hsnCode: "",
     companyName: "",
     batchNumber: 0,
+    expiryDate:'',
     availableQuantity: 0,
     scheme: 0,
     purchaseRate: 0,
@@ -148,31 +213,40 @@ const CreateOrder = () => {
                         id="batchNumber"
                       />
                     <Field
+                        name={`itemList.${index}.expiryDate`}
+                        component={inputDateField}
+                        required
+                        clearable={true}
+                        label=""
+                        format="dd/MM/yyyy"
+                      />
+                    <Field
                         name={`itemList.${index}.availableQuantity`}
                         label="Available Quantity"
-                      type="number"
-                        component={inputTextField}
+                        type="number"
+                        required
+                        component={inputNumberField}
                         index={index}
                         id="availableQuantity"
                       />
                     <Field
                         name={`itemList.${index}.scheme`}
                         label="Scheme"
-                        component={inputTextField}
+                        component={inputNumberField}
                         index={index}
                         id="scheme"
                       />
                     <Field
                         name={`itemList.${index}.purchaseRate`}
                         label="Purchase Rate"
-                        component={inputTextField}
+                        component={inputNumberField}
                         index={index}
                         id="purchaseRate"
                       />
                     <Field
                         name={`itemList.${index}.salesRate`}
                         label="Sales Rate"
-                        component={inputTextField}
+                        component={inputNumberField}
                         index={index}
                         id="salesRate"
                       />
