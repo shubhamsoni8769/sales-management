67eefa81886c3af8efe2d245bd0c2f60256ab256
AbhiBajpaishek <abhishekbajpai449@gmailcom>
2023-02-24 21:09:00 +0530

    Vendor schema and api added

--- server/app.ts
+++ server/app.ts
@@ -1,14 +1,11 @@
 import express from 'express';
 import morgan from 'morgan';
-import { addHsnCode, getHsnCode } from './controllers/hsnController.js';
-import { addCompany, getAllCompany } from './controllers/companyController.js';
-import { addMedicine, getAllMedicine, getMedicineByName } from './controllers/medicineController.js';
 import { errorMiddleware } from './middlewares/errorMiddleware.js';
 import { hsnCodeRouter } from './routes/hsnRoutes.js';
 import { companyRouter } from './routes/companyRoutes.js';
 import { medicineRouter } from './routes/medicineRoutes.js';
-import { BadRequestError } from './errors/ApiError.js';
 import { NotFoundError } from './errors/NotFoundError.js';
+import { vendorRouter } from './routes/vendorRoutes.js';
 
 
 export const app = express();
@@ -25,8 +22,10 @@ app.use('/api',hsnCodeRouter);
 app.use('/api', companyRouter);
 
 // Medicine
-
 app.use('/api', medicineRouter);
+
+// Vendor
+app.use('/api', vendorRouter);
 
 
 app.all('*', (_, __, next) => {
